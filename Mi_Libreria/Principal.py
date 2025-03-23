cba54abac702fa09a0a86a26b1d0d347158d28de
Maximiliano <tuemail@example.com>
2025-03-22 22:30:14 -0300

    2

--- Mi_Libreria/Principal.py
+++ Mi_Libreria/Principal.py
@@ -21,3 +21,49 @@ def Comunes(lista1, lista2, nombre="Key"):
     reglas = ["OK" if valor in set1 and valor in set2 else "L1" if valor in set1 else "L2" for valor in todos_los_valores]
     
     return pd.DataFrame({nombre: list(todos_los_valores), "Regla": reglas})
+
+def Resumen_columnas(tabla):
+    Resumen = pd.DataFrame(columns=["Columna", "Cantidad", "Elementos"])
+    for x in tabla.columns:
+        try:
+            elemento, cantidad = np.unique(tabla[x].values, return_counts=True)
+            Resumen.loc[len(Resumen)] = [x, len(elemento), elemento]
+        except:
+            Resumen.loc[len(Resumen)] = [x, 0, "Error"]
+    print(
+        "hola"
+    )
+    return Resumen.sort_values("Cantidad", ascending=False)
+
+
+# Función para añadir clave y/o índice a una tabla
+def Añadir_key_and_Indice(tabla, columna="Key", Key=True, Indice=False):
+    tabla = tabla.copy()
+    if Key:
+        tabla[columna] = tabla.apply(lambda row: ' '.join(row.astype(str)), axis=1)
+        tabla[columna] = tabla[columna].str.strip().str.lower()
+    if Indice:
+        tabla["Indice"] = tabla.groupby(columna).cumcount() + 1
+        new_col = f"{columna}2"
+        tabla[new_col] = tabla["Indice"].astype(str) + "-" + tabla[columna]
+        tabla.drop(columns=["Indice"], inplace=True)
+    return tabla
+
+# Función para identificar nuevos registros en una tabla comparada con otra
+def nuevo_registros(Tabla_nueva, Tabla_antigua):
+    col_nueva = Tabla_nueva.columns.values
+    col_original = Tabla_antigua.columns.values
+
+    if len(col_nueva) == len(col_original):
+        tab_nueva = Añadir_key_and_Indice(Tabla_nueva, Indice=True)
+        tab_original = Añadir_key_and_Indice(Tabla_antigua, Indice=True)
+        
+        l = Comunes(tab_nueva["Key2"], tab_original["Key2"]).rename(columns={"Key": "Key2"})
+        new = l[l["Regla"] == "L1"].merge(tab_nueva, on="Key2", how="left")
+        return new[col_nueva]
+    else:
+        print("Tablas distintas")
+        return None
+    
+    #Comit
+
